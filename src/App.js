a932bb21e16aaf2384b82d64fdd4c3c359be4876
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-29 14:10:11 -0400

    routes practice using useParam hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import { useParams } from "react-router";
+
+const Home = (props) => {
+  const { wordOrNumber, color1, color2 } = useParams();
+  console.log(wordOrNumber, color1, color2);
+  let jsx;
+  if (wordOrNumber) {
+    if (wordOrNumber === "home") {
+      jsx = <h1>Welcome</h1>;
+    } else if (isNaN(wordOrNumber)) {
+      color1 && color2
+        ? (jsx = (
+            <h1
+              style={{
+                color: `${color1}`,
+                backgroundColor: `${color2}`,
+                padding: "0.5rem",
+              }}
+            >
+              The word is: {wordOrNumber}
+            </h1>
+          ))
+        : (jsx = <h1>The word is: {wordOrNumber}</h1>);
+    } else {
+      jsx = <h1>The number is: {wordOrNumber}</h1>;
+    }
+  }
+  return <div style={{ textAlign: "center" }}>{jsx}</div>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <p>
+        <Link to="/home">Home</Link>
+        &nbsp;|&nbsp;
+        <Link to="/4">number</Link>
+        &nbsp;|&nbsp;
+        <Link to="/noon">word</Link>
+        &nbsp;|&nbsp;
+        <Link to="/pug/saddleBrown/tan">word and color</Link>
+      </p>
+      <Switch>
+        <Route path="/:wordOrNumber/:color1/:color2">
+          <Home />
+        </Route>
+        <Route path="/:wordOrNumber">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
